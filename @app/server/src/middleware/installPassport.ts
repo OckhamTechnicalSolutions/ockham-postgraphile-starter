103265ecf6ca77b6417b9adc3c3daae7ca60ed89
Todd Anderson <tsa@ockhamts.com>
2021-09-17 18:28:38 +0100

    working on google auth

--- @app/server/src/middleware/installPassport.ts
+++ @app/server/src/middleware/installPassport.ts
@@ -71,8 +71,7 @@ export default async (app: Express) => {
       {
         clientID: process.env.GOOGLE_KEY,
         clientSecret: process.env.GOOGLE_SECRET,
-        scope: ["user:email"],
-        callbackURL: "http://localhost:5678/auth/google/callback",
+        scope: ["profile"],
       },
       {},
       async (profile, _accessToken, _refreshToken, _extra, _req) => ({
